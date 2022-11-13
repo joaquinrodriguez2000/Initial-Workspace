4b383000cc2321f345dedf16d31846e3580a28c6
joaquinrodriguez2000 <joaropu@gmail.com>
2022-11-13 20:28:39 -0300

    Entrega 6
    Entrega 6 (carrito con error de Validación)

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,26 @@
 const BUYING_CART_INFO_URL = `https://japceibal.github.io/emercado-api/user_cart/25801.json`;
 
 let cartProduct = [];
+let DOLLAR_SYMBOL = "USD ";
+updateSubtotal = 0;
+shippingPercentage = 0.15;
+
+//Función que se utiliza para actualizar los costos de publicación
+function updateTotalCosts(){
+  let subtotalCostHTML = document.getElementById("subtotalText");
+  let shippingCostHTML = document.getElementById("shippingCostText");
+  let totalCostHTML = document.getElementById("totalCostCartText");
+
+  let subtotalCostToShow = DOLLAR_SYMBOL + updateSubtotal;
+  let shippingCostToShow = DOLLAR_SYMBOL + Math.round(updateSubtotal * shippingPercentage);
+  let totalCostToShow = DOLLAR_SYMBOL + (Math.round((updateSubtotal * shippingPercentage) + updateSubtotal));
+
+  subtotalCostHTML.innerHTML = subtotalCostToShow;
+  shippingCostHTML.innerHTML = shippingCostToShow;
+  totalCostHTML.innerHTML = totalCostToShow;
+}
+
+
 
 fetch (BUYING_CART_INFO_URL)
 .then(function(response) {
@@ -28,16 +48,81 @@ let cartToAppend = '';
 
 document.getElementById("buyingCart").innerHTML = cartToAppend
 
+
 function subTotal(amount) {
-    document.getElementById("subT").innerHTML = `${cart.currency} ${amount * cart.unitCost}`
+    
+  document.getElementById("subT").innerHTML = `${cart.currency} ${amount * cart.unitCost}`
+  updateSubtotal = amount * cart.unitCost;
+  updateTotalCosts();
+
 }
+
 document.getElementById("countInput").addEventListener("change", function(){ 
     amount = this.value;
     subTotal(amount);
-})
-}
 });
 
+document.getElementById("envioPremium").addEventListener("change", function(){
+  comissionPercentage = 0.15;
+  updateCosts();
+});
+
+document.getElementById("envioExpress").addEventListener("change", function(){
+  comissionPercentage = 0.07;
+  updateCosts();
+});
+
+document.getElementById("envioStandard").addEventListener("change", function(){
+  comissionPercentage = 0.05;
+  updateCosts();
+});
+
+
+
+}
+
+});
+
+
+
+
+    function validityForm() {
+    iguales = 1;
+      let  forms = document.querySelectorAll('.needs-validation')
+      Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+          form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault()
+              event.stopPropagation()
+            }
+    
+            form.classList.add('was-validated')
+          }, false)
+        }) 
+
+    let street = document.getElementById('streetRecipient').value;
+    let number = document.getElementById('numberRecipient').value;
+    let corner = document.getElementById('cornerRecipient').value;
+    let countRecipient  = document.getElementById('countInput').value;
+    console.log(street, number, corner, countRecipient);
+
+    if (street.length == 0 || number.length == 0 || corner.length == 0 || countRecipient == 0 ) {
+  
+      return;
+    }
+     else if (document.getElementById("envioPremium").checked === false || document.getElementById("envioExpress").checked === false || document.getElementById("envioExpress").checked === false) {
+  
+      return ; 
+      
+}     
+
+  else {
+        alert("¡Has comprado con éxito!");    
+  } 
+
+  
+}
 
 
 
